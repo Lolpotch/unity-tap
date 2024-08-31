ced8607ddb8320712ebe565734b20cf5c0e8989e
Chandra Tritaqwa Ramadhan <107376090+Lolpotch@users.noreply.github.com>
2024-08-31 17:15:23 +0700

    Add files via upload

---
+++ Assets/ChangingImage.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangingImage : MonoBehaviour
+{
+    Image image;
+    public Sprite[] images;
+
+    void Awake()
+    {
+        image = GetComponent<Image>();
+    }
+
+    //Animation event
+    public void Changing()
+    {
+        if (image.sprite == images[0])
+        {
+            image.sprite = images[1];
+        }
+        else
+        {
+            image.sprite = images[0];
+        }
+    }
+}
---
+++ Assets/DevIntro.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DevIntro : MonoBehaviour
+{
+    //Animation Events
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
